002fb6b785255ed0788c5a96a965feacc3cb7e5a
名宏 鄭 <free576002@gmail.com>
2018-03-06 12:24:15 +0800

    20180306
    Update Commit

---
+++ Minesweeper_Alpha/DataModels.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Minesweeper_Alpha
+{
+    public class MinesweeperConfig
+    {
+        public List<BoomsPosition> _BoomsPosition = new List<BoomsPosition>();
+        public List<NumberPosition> _NumberPosition = new List<NumberPosition>();
+        public List<SaveAreaPosition> _SaveAreaPosition = new List<SaveAreaPosition>();
+        public List<InitXY> InitXY = new List<InitXY>();
+    }
+
+    public class InitXY
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+    }
+
+    public class BoomsPosition
+    {
+        public int Booms_x { get; set; }
+
+        public int Booms_y { get; set; }
+    }
+
+    public class NumberPosition
+    {
+        public int Number_x { get; set; }
+
+        public int Number_y { get; set; }
+    }
+
+    public class SaveAreaPosition
+    {
+        public int SaveArea_x { get; set; }
+
+        public int SaveArea_y { get; set; }
+    }
+}
--- Minesweeper_Alpha/Program.cs
+++ Minesweeper_Alpha/Program.cs
@@ -19,11 +19,9 @@ namespace Minesweeper_Alpha
         static string[,] BoomsCanvas = new string[Position_x, Position_y];
 
         //炸彈位置
-        List<BoomsPosition> _BoomsPosition = new List<BoomsPosition>();
-        List<NumberPosition> _NumberPosition = new List<NumberPosition>();
-        List<SaveAreaPosition> _SaveAreaPosition = new List<SaveAreaPosition>();
+        MinesweeperConfig _MinesweeperConfig;
 
-        List<CheckArea> _CheckArea = new List<CheckArea>();
+
         static void Main(string[] args)
         {
             Program _Program = new Program();
@@ -39,36 +37,76 @@ namespace Minesweeper_Alpha
                     string CheckXY = Console.ReadLine();
                     var x = CheckXY.Split(',')[0];
                     var y = CheckXY.Split(',')[1];
-                    _Program.SeedFill(int.Parse(x), int.Parse(y));
+                    _Program.CheckPlaid(int.Parse(x), int.Parse(y));
                     _Program.ShowCanvas();
-                    Console.ReadLine();
                 }
-            }
-            
+            }    
         }
 
-        public void ShowCanvas()
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void GameInit()
         {
-            //Show
-            for (int x = 0; x < Position_x; x++)
-            {
-                for (int y = 0; y < Position_y; y++)
-                {
-                    Console.Write("  " + BoomsCanvas[x, y]);
-                }
-                Console.Write("\n\r");
-            }
+            Array.Clear(BoomsCanvas,0, BoomsCanvas.Length);
+            _MinesweeperConfig = new MinesweeperConfig();
+
+            this.SetBooms();
+            this.SetPointNumber();
+            this.SetSaveArea();
         }
+
+        /// <summary>
+        /// 確認選擇的地方是炸彈或數字,來決定下一步該如何執行
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void CheckPlaid(int x, int y)
+        {
+            var BoomsQuery = _MinesweeperConfig._BoomsPosition.Where(o => o.Booms_x == x && o.Booms_y == y);
+            if (BoomsQuery.Any())
+            {
+                Console.WriteLine("Game Over \r\n 遊戲刷新");
+                GameInit();
+                return;
+            }
+            var NumberPointQuery = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x && o.Number_y == y);
+            if (NumberPointQuery.Any())
+            {
+                if (_MinesweeperConfig._NumberPosition.Count == 1)
+                {
+                    Console.WriteLine("You Win \r\n 遊戲刷新");
+                    GameInit();
+                    return;
+                }
+                else
+                {
+                    BoomsCanvas[x, y] = "0";
+                    _MinesweeperConfig._NumberPosition.Remove(NumberPointQuery.FirstOrDefault());
+                }
+                
+                return;
+            }
+            
+            SeedFill(x,y);
+        }
+                /// <summary>
+        /// 利用遞迴尋找以使用者選擇的座標為中心,利用Fill找出其SaveArea區域
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public void SeedFill(int x, int y)
         {
-            var Query = _SaveAreaPosition.Where(o => o.SaveArea_x == x && o.SaveArea_y == y).FirstOrDefault();
-            bool AnyQuery = _SaveAreaPosition.Where(o => o.SaveArea_x == x && o.SaveArea_y == y).Any();
-            _SaveAreaPosition.Remove(Query);
+            var Query = _MinesweeperConfig._SaveAreaPosition.Where(o => o.SaveArea_x == x && o.SaveArea_y == y).FirstOrDefault();
+            bool AnyQuery = _MinesweeperConfig._SaveAreaPosition.Where(o => o.SaveArea_x == x && o.SaveArea_y == y).Any();
+            _MinesweeperConfig._SaveAreaPosition.Remove(Query);
 
             //Open         
             if (AnyQuery)
             {
-                AroundOpen(x, y);
+                CheckAround(x, y);
+
+                //利用遞迴檢查SaveArea自身及上下左右
                 SeedFill(x, y);
                 SeedFill(x - 1, y);
                 SeedFill(x + 1, y);
@@ -77,71 +115,75 @@ namespace Minesweeper_Alpha
             }
         }
 
-        public void AroundOpen( int x, int y)
+        /// <summary>
+        /// 以SaveArea座標為中心,尋找其九宮格是否有數字
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void CheckAround( int x, int y)
         {
             //上下左右
-            var top = _NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y);
-            var down = _NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y);
-            var left = _NumberPosition.Where(o => o.Number_x == x && o.Number_y == y - 1);
-            var right = _NumberPosition.Where(o => o.Number_x == x && o.Number_y == y + 1);
+            var top = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y);
+            var down = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y);
+            var left = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x && o.Number_y == y - 1);
+            var right = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x && o.Number_y == y + 1);
 
             //左上 左下 右上 右下
-            var tl = _NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y - 1);
-            var tr = _NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y - 1);
-            var dl = _NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y + 1);
-            var dr = _NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y + 1);
+            var tl = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y - 1);
+            var tr = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y - 1);
+            var dl = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x - 1 && o.Number_y == y + 1);
+            var dr = _MinesweeperConfig._NumberPosition.Where(o => o.Number_x == x + 1 && o.Number_y == y + 1);
 
             //上下左右
             if (top.Any())
             {
                 BoomsCanvas[x - 1, y] = "x";
-                _NumberPosition.Remove(top.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(top.FirstOrDefault());
             }
             if (down.Any())
             {
                 BoomsCanvas[x + 1, y] = "x";
-                _NumberPosition.Remove(down.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(down.FirstOrDefault());
             }
             if (left.Any())
             {
                 BoomsCanvas[x, y - 1] = "x";
-                _NumberPosition.Remove(left.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(left.FirstOrDefault());
             }
             if (right.Any())
             {
                 BoomsCanvas[x, y + 1] = "x";
-                _NumberPosition.Remove(right.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(right.FirstOrDefault());
             }
 
             if (tl.Any())
             {
                 BoomsCanvas[x - 1, y - 1] = "x";
-                _NumberPosition.Remove(tl.FirstOrDefault());
-            }
+                _MinesweeperConfig._NumberPosition.Remove(tl.FirstOrDefault());
+            }      
+
             if (tr.Any())
             {
                 BoomsCanvas[x + 1, y - 1] = "x";
-                _NumberPosition.Remove(tr.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(tr.FirstOrDefault());
             }
+
             if (dl.Any())
             {
                 BoomsCanvas[x - 1, y + 1] = "x";
-                _NumberPosition.Remove(dl.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(dl.FirstOrDefault());
             }
+
             if (dr.Any())
             {
                 BoomsCanvas[x + 1, y + 1] = "x";
-                _NumberPosition.Remove(dr.FirstOrDefault());
+                _MinesweeperConfig._NumberPosition.Remove(dr.FirstOrDefault());
             }
         }
 
-        public void GameInit()
-        {
-            this.SetBooms();
-            this.SetPointNumber();
-            this.SetSaveArea();
-        }
-
+        /// <summary>
+        /// 設置炸彈
+        /// </summary>
         public void SetBooms()
         {     
             Random _Random = new Random(Guid.NewGuid().GetHashCode());
@@ -159,7 +201,7 @@ namespace Minesweeper_Alpha
                     if (string.IsNullOrEmpty(BoomsCanvas[BoomsPosition_x, BoomsPosition_y]))
                     {
                         BoomsCanvas[BoomsPosition_x, BoomsPosition_y] = string.Format("*");
-                        _BoomsPosition.Add(new BoomsPosition()
+                        _MinesweeperConfig._BoomsPosition.Add(new BoomsPosition()
                         {
                             Booms_x = BoomsPosition_x,
                             Booms_y = BoomsPosition_y
@@ -170,10 +212,13 @@ namespace Minesweeper_Alpha
             }
         }
 
+        /// <summary>
+        /// 設置提示數字
+        /// </summary>
         public void SetPointNumber()
         {
-            #region 以炸彈為九宮格
-            foreach (var _p in _BoomsPosition)
+            #region 以炸彈為中心九宮格
+            foreach (var _p in _MinesweeperConfig._BoomsPosition)
             {
                 //1,1
                 //00 10 20
@@ -218,6 +263,10 @@ namespace Minesweeper_Alpha
             }
             #endregion
         }
+
+        /// <summary>
+        /// 設置安全區域
+        /// </summary>
         public void SetSaveArea()
         {
             for (int x = 0; x < Position_x; x++)
@@ -227,7 +276,7 @@ namespace Minesweeper_Alpha
                     if (string.IsNullOrEmpty(BoomsCanvas[x,y]))
                     {
                         BoomsCanvas[x, y] = string.Format("-");
-                        _SaveAreaPosition.Add(new SaveAreaPosition()
+                        _MinesweeperConfig._SaveAreaPosition.Add(new SaveAreaPosition()
                         {
                             SaveArea_x = x,
                             SaveArea_y = y
@@ -237,13 +286,18 @@ namespace Minesweeper_Alpha
             }
         }
 
-        //提示炸彈數量確認
+        /// <summary>
+        /// 提示數字計算
+        /// </summary>
+        /// <param name="BoomX"></param>
+        /// <param name="BoomY"></param>
+        /// <returns></returns>
         public string BoomsAmount(int BoomX, int BoomY)
         {
             string BoomsPosition = BoomsCanvas[BoomX, BoomY];
             if (string.IsNullOrEmpty(BoomsPosition))
             {
-                _NumberPosition.Add(new NumberPosition()
+                _MinesweeperConfig._NumberPosition.Add(new NumberPosition()
                 {
                     Number_x = BoomX,
                     Number_y = BoomY
@@ -257,31 +311,20 @@ namespace Minesweeper_Alpha
             return (int.Parse(BoomsPosition) + 1).ToString();
         }
 
-        public class BoomsPosition
+        /// <summary>
+        /// 畫面顯示
+        /// </summary>
+        public void ShowCanvas()
         {
-            public int Booms_x { get; set; }
-
-            public int Booms_y { get; set; }
-        }
-
-        public class NumberPosition
-        {
-            public int Number_x { get; set; }
-
-            public int Number_y { get; set; }
-        }
-
-        public class SaveAreaPosition
-        {
-            public int SaveArea_x { get; set; }
-
-            public int SaveArea_y { get; set; }
-        }
-        public class CheckArea
-        {
-            public int CheckArea_x { get; set; }
-
-            public int CheckArea_y { get; set; }
+            //Show
+            for (int x = 0; x < Position_x; x++)
+            {
+                for (int y = 0; y < Position_y; y++)
+                {
+                    Console.Write("  " + BoomsCanvas[x, y]);
+                }
+                Console.Write("\n\r");
+            }
         }
     }
 }
